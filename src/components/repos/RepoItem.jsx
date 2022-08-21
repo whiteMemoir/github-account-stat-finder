1b022764008b72b6fd6b215c1d76743782d59878
namikazepian <namikazepian@gmail.com>
2022-08-21 15:25:43 +0700

    repo component done

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,7 +1,44 @@
 import PropTypes from "prop-types";
+import { FaEye, FaInfo, FaStar, FaLink, FaUtensils } from "react-icons/fa";
 
 const RepoItem = ({ repo }) => {
-	return <div>RepoItem</div>;
+	const {
+		name,
+		description,
+		html_url,
+		stargazers_count,
+		watchers_count,
+		open_issues,
+		forks,
+	} = repo;
+
+	return (
+		<div className="mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
+			<div className="card-body">
+				<h3 className="mb-2 text-xl font-semibold">
+					<a href={html_url}>
+						<FaLink className="inline mr-1" /> &nbsp;
+						{name}
+					</a>
+				</h3>
+				<div className="mb-3 ">{description}</div>
+				<div>
+					<div className="mr-2 badge badge-outline badge-info badge-lg">
+						<FaEye className="mr-2" /> &nbsp; {watchers_count}
+					</div>
+					<div className="mr-2 badge badge-outline badge-warning badge-lg">
+						<FaStar className="mr-2" /> &nbsp; {stargazers_count}
+					</div>
+					<div className="mr-2 badge badge-outline badge-success badge-lg">
+						<FaUtensils className="mr-2" /> &nbsp; {forks}
+					</div>
+					<div className="mr-2 badge badge-outline badge-error badge-lg">
+						<FaInfo className="mr-2" /> &nbsp; {open_issues}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 RepoItem.propTypes = {
